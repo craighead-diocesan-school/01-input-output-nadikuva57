6dffd573c04c22d5f5c15613dacc1d32b40e3059
Varuni <nadikuva57@craighead.school.nz>
2023-05-23 10:43:57 +1200

    Wrote the best joke in the world

--- code/0-greeting.js
+++ code/0-greeting.js
@@ -10,4 +10,6 @@ function greeting() {
 
 function joke() {
   // write the code for your joke here.
+  let joke = prompt("wanna hear a joke?")
+  alert("Listening to people is like reading to terms & conditions of website. You understand, still you say... 'I agree'")
 }
